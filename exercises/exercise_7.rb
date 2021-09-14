31621fdfd97b9111b869691d56196a4a197e4f53
Brian Sohn <kolpp15@gmail.com>
2021-09-13 23:31:22 -0700

    Exercise 7: Validation and user create a store feature

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts ">>> ENTER A STORE NAME <<<"
+print ">>> "
+user_new = gets.chomp
+@add_store  = Store.create(name: user_new)
+
+#PRINT ERRORS: 
+puts ">>> ERROR (Add products): #{@add_store.errors[:min_product]}"
+puts ">>> ERROR (Annual Revenue): #{@add_store.errors[:annual_revenue]}"
+
+#CHECK TOTAL STORES:
+@total_stores   = Store.count
+puts ">>>Total stores in db: #{@total_stores}"
